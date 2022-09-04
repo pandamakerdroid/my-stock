e8f99fe84984a11eb1fbdc920e5b881cadafa86b
Simin Cao <simin.cao@wu.ac.at>
2022-09-04 19:37:00 +0200

    use empty component for WIP routes

--- frontend/src/shared/components/Body.tsx
+++ frontend/src/shared/components/Body.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import FAQ from '@views/FAQ';
 import MyStocks from '@views/MyStocks';
 import Container from '@mui/material/Container';
+import MyCryptos from '@views/MyCryptos';
 
 function Body(props:any) {
     return (
@@ -10,6 +11,8 @@ function Body(props:any) {
           <Route path="/" element={<MyStocks/>}></Route>
           <Route path="/stocks" element={<MyStocks/>}></Route>
           <Route path="/faq" element={<FAQ/>}></Route>
+          <Route path="/cryptos" element={<MyCryptos/>}></Route>
+          <Route path="/contact" element={<MyCryptos/>}></Route>
 
         </Routes>
       </Container>
